93309d2b88ad50999b320b5522d875ca4d71b6cf
Abhi Koradia <abhikoradia@gmail.com>
2021-08-30 11:34:48 +0530

    Updates

---
+++ precache-manifest.c32dcb82d77d872206a7c1fed4ccbddd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f70cb78d3a6e492246a38cbfcc9aba",
+    "url": "/Product-Card/index.html"
+  },
+  {
+    "revision": "e9012638461b7988eabf",
+    "url": "/Product-Card/static/css/main.fe36e714.chunk.css"
+  },
+  {
+    "revision": "c7e41bb00b2f7a59004c",
+    "url": "/Product-Card/static/js/2.487cbfb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Product-Card/static/js/2.487cbfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9012638461b7988eabf",
+    "url": "/Product-Card/static/js/main.ef06ebb5.chunk.js"
+  },
+  {
+    "revision": "d43f446c622eba76e21d",
+    "url": "/Product-Card/static/js/runtime-main.0f4eb200.js"
+  },
+  {
+    "revision": "f49f54e428c6cc85b74409aa68254fb1",
+    "url": "/Product-Card/static/media/black.f49f54e4.png"
+  },
+  {
+    "revision": "182fd0442a5ee9c468c3695506fc8d69",
+    "url": "/Product-Card/static/media/blue.182fd044.png"
+  },
+  {
+    "revision": "88f61bc52478d5190ee00d7b81198ef3",
+    "url": "/Product-Card/static/media/green.88f61bc5.png"
+  },
+  {
+    "revision": "b3434e3e703dc1cefa6052ace31234c1",
+    "url": "/Product-Card/static/media/logo.b3434e3e.png"
+  },
+  {
+    "revision": "0e59d7df6292539663350c2ccb7b3cc2",
+    "url": "/Product-Card/static/media/orange.0e59d7df.png"
+  },
+  {
+    "revision": "60bfc4f39a5c7b5a6ac4b9769ebed74b",
+    "url": "/Product-Card/static/media/red.60bfc4f3.png"
+  }
+]);
